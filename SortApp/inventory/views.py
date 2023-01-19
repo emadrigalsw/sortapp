623d3d02ff1f9ecb0198bfc92e9623866f4e59b8
emadrigalsw <121644810+emadrigalsw@users.noreply.github.com>
2023-01-19 13:37:16 -0500

    Create a basic home page with a  link to home in base.html

--- SortApp/inventory/views.py
+++ SortApp/inventory/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = {'text': 'Welcome to SortApp, your Deli manager'}
+    return render(request, 'inventory/home.html', context)
